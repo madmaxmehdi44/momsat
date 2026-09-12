797aa4b0f44909ae8b3e9538302ef9c41dfedb8e
Medi <madmaxmehdi44@gmail.com>
2026-09-12 04:59:01 +0330

    fix: allow persistent player on home and recent views

--- components/BrowsePlayerGuard.tsx
+++ components/BrowsePlayerGuard.tsx
@@ -1,18 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
-import { usePersistentPlayer } from './PersistentPlayerProvider';
-
 export default function BrowsePlayerGuard() {
-  const pathname = usePathname();
-  const { stopPlayer } = usePersistentPlayer();
-
-  useEffect(() => {
-    if (pathname !== '/') return;
-    const timer = window.setTimeout(() => stopPlayer(), 0);
-    return () => window.clearTimeout(timer);
-  }, [pathname, stopPlayer]);
-
   return null;
 }
