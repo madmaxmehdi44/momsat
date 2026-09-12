360ead18a470b1accf2d8e817b674e9c4d728eef
Medi <madmaxmehdi44@gmail.com>
2026-09-12 03:49:05 +0330

    refactor: scope homepage player guard to root

--- components/BrowsePlayerGuard.tsx
+++ components/BrowsePlayerGuard.tsx
@@ -9,7 +9,7 @@ export default function BrowsePlayerGuard() {
   const { stopPlayer } = usePersistentPlayer();
 
   useEffect(() => {
-    if (pathname !== '/browse' && pathname !== '/') return;
+    if (pathname !== '/') return;
     const timer = window.setTimeout(() => stopPlayer(), 0);
     return () => window.clearTimeout(timer);
   }, [pathname, stopPlayer]);
